279ed8143ea46e2fd30e1fb95d136e705b815a32
informatics <informatics@informaticss-Mac-mini.local>
2019-03-13 11:41:17 +0700

    Initial Commit

---
+++ JeffBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JeffBrowser
+//
+//  Created by informatics on 13/3/2562 BE.
+//  Copyright © 2562 Jeffware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
