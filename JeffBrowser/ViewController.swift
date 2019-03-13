2b338a3223f677b14b3b48152e60f767dc506b33
Kraiwich <le_kraiwich@hotmail.com>
2019-03-13 14:12:16 +0700

    add search field and search function

--- JeffBrowser/ViewController.swift
+++ JeffBrowser/ViewController.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKUIDelegate {
 
+    @IBOutlet weak var webView : WKWebView!
+    @IBOutlet weak var searchField : UITextField!
+    
+    @IBOutlet weak var backButton : UIButton!
+    @IBOutlet weak var forwardButton : UIButton!
+    
+    var history = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        searchField.text! = "www.google.com"
+        search(strUrl : searchField.text!)
     }
-
+    
+    func search(strUrl : String) {
+        
+        history.append(strUrl)
+        
+        let myURL = URL(string:"https://\(strUrl)")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+    }
+    
+    @IBAction func searchButtonCLick() {
+        search(strUrl : searchField.text!)
+    }
+    
+    @IBAction func backButtonClick() {
+        search(strUrl : history.popLast()!)
+    }
 
 }
 
